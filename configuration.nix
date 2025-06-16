5026754eb5678caa4b53bfacb41654b4c4779116
MaxHearnden <maxoscarhearnden@gmail.com>
2025-06-16 14:03:49 +0000

    Enable knot

--- configuration.nix
+++ configuration.nix
@@ -1,7 +1,18 @@
-{ config, pkgs, ... }: {
+{ config, lib, pkgs, ... }: {
   boot = {
+    initrd.systemd.enable = true;
     kernelPackages = pkgs.linuxPackages_latest;
     loader.systemd-boot.enable = true;
+  };
+  environment.etc = {
+    "knot/zandoodle.me.uk.zone".text = ''
+      $TTL 600
+      @ SOA dns mail 0 600 60 3600 600
+      @ NS dns
+      @ CAA 128 issue ";"
+      $INCLUDE /var/lib/ddns/zonefile
+      $INCLUDE /var/lib/ddns/zonefile dns.zandoodle.me.uk.
+    '';
   };
   fileSystems = {
     "/" = {
@@ -62,6 +73,23 @@
   services = {
     knot = {
       enable = true;
+      settings = {
+        server = {
+          listen = ["0.0.0.0" "::"];
+        };
+        zone = [
+          {
+            dnssec-signing = true;
+            domain = "zandoodle.me.uk";
+            file = "/etc/knot/zandoodle.me.uk.zone";
+            semantic-checks = true;
+            journal-content = "all";
+            zonefile-load = "difference-no-serial";
+            zonemd-generate = "zonemd-sha512";
+            zonefile-sync = -1;
+          }
+        ];
+      };
     };
     openssh = {
       enable = true;
@@ -83,18 +111,85 @@
       flags = [ "--no-write-lock-file" ];
       flake = "git+file:///home/max/nixos-config";
     };
+    etc.overlay.enable = true;
     stateVersion = "24.11";
   };
-  systemd.shutdownRamfs.enable = false;
-  users.users.max = {
-    isNormalUser = true;
-    extraGroups = [ "wheel" ];
-    openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILmioGtxIY2vgxZi5czG/tIkSKga/91RDyTsNtc6fU3D max@max-nixos-pc"
-    ];
-    packages = with pkgs; [
-      btop
-      htop
-    ];
+  systemd = {
+    services = {
+      get-IP-address = {
+        confinement.enable = true;
+        serviceConfig = {
+          CapabilityBoundingSet = "";
+          Group = "ddns";
+          IPAddressAllow = "192.168.1.1";
+          IPAddressDeny = "any";
+          LockPersonality = true;
+          MemoryDenyWriteExecute = true;
+          NoNewPrivileges = true;
+          ProcSubset = "pid";
+          ProtectClock = true;
+          ProtectHome = true;
+          ProtectHostname = true;
+          ProtectKernelLogs = true;
+          ProtectProc = "invisible";
+          ProtectSystem = "strict";
+          RemoveIPC = true;
+          RestrictAddressFamilies = "AF_INET";
+          RestrictRealtime = true;
+          RestrictSUIDSGID = true;
+          RuntimeDirectory = "ddns";
+          StateDirectory = "ddns";
+          SystemCallArchitectures = "native";
+          SystemCallFilter = [ "@system-service" "~@privileged @resources" ];
+          Type = "oneshot";
+          User = "ddns";
+        };
+        script = ''
+          set -x
+          ${lib.getExe pkgs.curl} -o /run/ddns/login.lp -v \
+            http://192.168.1.1/login.lp?getSessionStatus=true
+          ${lib.getExe pkgs.jq} -r .wanIPAddress /run/ddns/login.lp \
+            >/run/ddns/IPv4-address
+          printf "@ A " | ${lib.getExe' pkgs.coreutils "cat"} - /run/ddns/IPv4-address >/run/ddns/zonefile
+          ${lib.getExe' pkgs.ldns.examples "ldns-read-zone"} -E A -c /run/ddns/zonefile >/run/ddns/zonefile-canonical
+          record_count=$(${lib.getExe' pkgs.coreutils "wc"} -l --total=only /run/ddns/zonefile-canonical)
+          if [ "$record_count" != 1 ]; then
+            echo "Potential attack detected" >&2
+            exit 1
+          fi
+
+          ${lib.getExe' pkgs.coreutils "mv"} -f /run/ddns/IPv4-address /run/ddns/zonefile /var/lib/ddns/
+        '';
+      };
+      knot.reloadTriggers = [ config.environment.etc."knot/zandoodle.me.uk.zone".source ];
+    };
+    timers.get-IP-address = {
+      timerConfig = {
+        OnUnitActiveSec = "1h";
+      };
+      wantedBy = [ "timers.target" ];
+    };
+    shutdownRamfs.enable = false;
+  };
+  users = {
+    users = {
+      ddns = {
+        isSystemUser = true;
+        group = "ddns";
+      };
+      max = {
+        isNormalUser = true;
+        extraGroups = [ "wheel" ];
+        openssh.authorizedKeys.keys = [
+          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILmioGtxIY2vgxZi5czG/tIkSKga/91RDyTsNtc6fU3D max@max-nixos-pc"
+        ];
+        packages = with pkgs; [
+          btop
+          htop
+          dig
+        ];
+      };
+    };
+    groups.ddns = {};
   };
 }
