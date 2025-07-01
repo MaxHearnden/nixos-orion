03021987656e5eff01056748adbce556b239e80a
MaxHearnden <maxoscarhearnden@gmail.com>
2025-07-01 18:40:06 +0000

    Add cert name to TLSA zonefile

--- gen-TLSA.nix
+++ gen-TLSA.nix
@@ -11,6 +11,7 @@ runCommandNoCC
   }
   ''
     for name in "''${names[@]}"; do
+      echo "; $name" >>$out
       printf "@ TLSA 0 1 2 " >>$out
       openssl x509 -in ${cacert.unbundled}/etc/ssl/certs/"$name":*.crt -noout \
         -pubkey | openssl pkey -pubin -outform DER | openssl dgst -sha512 -hex |
