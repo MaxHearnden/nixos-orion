a02441a7ae23fb5f145cd2222e12d089f290c9f4
MaxHearnden <maxoscarhearnden@gmail.com>
2025-07-04 23:40:30 +0000

    Use fib to detemine whether the packet's destination is this host

--- configuration.nix
+++ configuration.nix
@@ -160,9 +160,9 @@
 
           chain dns-rd {
             type nat hook prerouting priority dstnat; policy accept;
-            pkttype host udp dport 53 @th,87,1 == 1 ip saddr @local_ip redirect to :55 comment "Recursion desired"
-            pkttype host udp dport 53 @th,87,1 == 1 ip6 saddr @local_ip6 redirect to :55 comment "Recursion desired"
-            pkttype host udp dport 53 redirect to :54 comment "Recursion not desired"
+            fib daddr . iif . mark type local udp dport 53 @th,87,1 == 1 ip saddr @local_ip redirect to :55 comment "Recursion desired"
+            fib daddr . iif . mark type local udp dport 53 @th,87,1 == 1 ip6 saddr @local_ip6 redirect to :55 comment "Recursion desired"
+            fib daddr . iif . mark type local udp dport 53 redirect to :54 comment "Recursion not desired"
           }
         '';
       };
