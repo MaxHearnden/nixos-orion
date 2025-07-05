84b4b2e50ddce7b787fe91a36395e777d4cd1285
MaxHearnden <maxoscarhearnden@gmail.com>
2025-07-05 20:03:15 +0000

    NAT DNS queries to addresses assigned to interfaces other than the incomming interface

--- configuration.nix
+++ configuration.nix
@@ -166,11 +166,11 @@
 
           chain dns-rd {
             type nat hook prerouting priority dstnat; policy accept;
-            fib daddr . iif . mark type local udp dport 53 @th,87,1 == 1 ip saddr @local_ip redirect to :55 comment "Recursion desired"
-            fib daddr . iif . mark type local udp dport 53 @th,87,1 == 1 ip6 saddr @local_ip6 redirect to :55 comment "Recursion desired"
-            fib daddr . iif . mark type local udp dport 53 redirect to :54 comment "Recursion not desired"
-            fib daddr . iif . mark type local tcp dport 53 ip saddr != @local_ip redirect to :54 comment "Tcp recursion not desired"
-            fib daddr . iif . mark type local tcp dport 53 ip6 saddr != @local_ip6 redirect to :54 comment "Tcp recursion not desired"
+            fib daddr . mark type local udp dport 53 @th,87,1 == 1 ip saddr @local_ip redirect to :55 comment "Recursion desired"
+            fib daddr . mark type local udp dport 53 @th,87,1 == 1 ip6 saddr @local_ip6 redirect to :55 comment "Recursion desired"
+            fib daddr . mark type local udp dport 53 redirect to :54 comment "Recursion not desired"
+            fib daddr . mark type local tcp dport 53 ip saddr != @local_ip redirect to :54 comment "Tcp recursion not desired"
+            fib daddr . mark type local tcp dport 53 ip6 saddr != @local_ip6 redirect to :54 comment "Tcp recursion not desired"
           }
 
           chain dns-rd-output {
