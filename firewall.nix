f19f6fa8efe1a62a7da61466be005b4d1f832fd8
MaxHearnden <maxoscarhearnden@gmail.com>
2026-01-02 17:36:07 +0000

    Move nftables.service into the firewall module

--- firewall.nix
+++ firewall.nix
@@ -217,6 +217,34 @@
       dnsmasq.serviceConfig.NFTSet = "cgroup:inet:services:dnsmasq";
       knot.serviceConfig.NFTSet = "cgroup:inet:services:knot";
       maddy.serviceConfig.NFTSet = "cgroup:inet:services:maddy";
+      nftables = {
+        confinement = {
+          enable = true;
+          packages = [ pkgs.coreutils ];
+        };
+        serviceConfig = {
+          DynamicUser = true;
+          AmbientCapabilities = "CAP_NET_ADMIN";
+          CapabilityBoundingSet = "CAP_NET_ADMIN";
+          IPAddressDeny = "any";
+          SystemCallFilter = [ "@system-service" "~@privileged @resources" ];
+          RestrictRealtime = true;
+          RestrictAddressFamilies = "AF_NETLINK";
+          RestrictNamespaces = true;
+          MemoryDenyWriteExecute = true;
+          SystemCallArchitectures = "native";
+          ProtectKernelLogs = true;
+          ProtectClock = true;
+          LockPersonality = true;
+          PrivateUsers = false;
+          ProtectHostname = true;
+          ProtectProc = "invisible";
+          ProcSubset = "pid";
+          User = "nft";
+          UMask = "077";
+          Group = "nft";
+        };
+      };
       sshd.serviceConfig.NFTSet = "cgroup:inet:services:sshd";
       systemd-networkd.serviceConfig.NFTSet = "cgroup:inet:services:systemd_networkd";
       tailscaled.serviceConfig.NFTSet = "cgroup:inet:services:tailscaled";
