74ead17d1f64e5cfe253132bd2c6923ec2c6777c
MaxHearnden <maxoscarhearnden@gmail.com>
2026-01-02 17:52:02 +0000

    Remove duplicate copy of nftables unit

--- configuration.nix
+++ configuration.nix
@@ -488,34 +488,6 @@ in
       wait-online.enable = false;
     };
     services = {
-      nftables = {
-        confinement = {
-          enable = true;
-          packages = [ pkgs.coreutils ];
-        };
-        serviceConfig = {
-          DynamicUser = true;
-          AmbientCapabilities = "CAP_NET_ADMIN";
-          CapabilityBoundingSet = "CAP_NET_ADMIN";
-          IPAddressDeny = "any";
-          SystemCallFilter = [ "@system-service" "~@privileged @resources" ];
-          RestrictRealtime = true;
-          RestrictAddressFamilies = "AF_NETLINK";
-          RestrictNamespaces = true;
-          MemoryDenyWriteExecute = true;
-          SystemCallArchitectures = "native";
-          ProtectKernelLogs = true;
-          ProtectClock = true;
-          LockPersonality = true;
-          PrivateUsers = false;
-          ProtectHostname = true;
-          ProtectProc = "invisible";
-          ProcSubset = "pid";
-          User = "nft";
-          UMask = "077";
-          Group = "nft";
-        };
-      };
       nscd.serviceConfig = {
         CapabilityBoundingSet = "";
         LockPersonality = true;
